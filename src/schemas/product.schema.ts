e1f7f9c8202ba450a32758df2391e9a00f408ecc
Alexander Justine Santiago <99528092+AJustiago@users.noreply.github.com>
2025-03-09 20:44:52 +0700

    Revert "fix: add product stuck"
    This reverts commit 1cec55b1bcae18988546661530c0647ddef325e4.

--- src/schemas/product.schema.ts
+++ src/schemas/product.schema.ts
@@ -2,10 +2,12 @@ import { z } from "zod";
 
 export const productSchema = z.object({
   id: z.number().optional(),
-  product_name: z.string(),
+  product_name: z.string().min(1, "Product name is required"),
   product_colorway: z.string()|| "",
   product_desc: z.string().optional(),
-  photo: z.string(),
+  photo: z.string().refine((val) => val.startsWith("/") || val.startsWith("http"), {
+    message: "Image path must start with '/' or be a valid URL",
+  }),
   price: z.number().min(0, "Price must be a positive number"),
   qty: z.number().int().min(0, "Quantity must be a non-negative integer"),
   functionEnabled: z.boolean().default(false),
