818a425abeb0bc51bb273e0c4951b955f44726ef
AJustiago <alexcode0702@gmail.com>
2025-04-04 17:09:49 +0700

    inprog: auth nestjs

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
     PrismaModule,
     AdminModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- front-end/src/services/loginService.ts
+++ front-end/src/services/loginService.ts
@@ -32,7 +32,7 @@ export function getToken(): string | null {
 export async function loginService(
   credentials: LoginCredentials
 ): Promise<LoginResponse> {
-  const response = await fetch("http://localhost:3100/auth/login", {
+  const response = await fetch("http://localhost:3100/admin", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(credentials),
